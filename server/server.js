ecdb8a971b31aa64883571e7e455d28b773111f3
brian watroba <brianwatroba@gmail.com>
2022-02-16 12:27:42 -0500

    server start

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+// const path = require("path");
+// const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Init middleware
+app.use(express.static("static"));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+
+// Define routes
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/entries', require('./routes/api/entries'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/sms', require('./routes/api/sms'));
+// app.use('/api/twofac', require('./routes/api/twofac'));
+
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
+
+module.exports = server;
