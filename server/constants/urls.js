3e873e28b99fac40837edbd3256001fcc0f987d9
brian watroba <brianwatroba@gmail.com>
2022-02-17 02:14:58 -0500

    auth middleware in progress

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,4 +1,6 @@
 const TWITCH_SUB_URL = `https://api.twitch.tv/helix/eventsub/subscriptions`;
+const TWITCH_AUTH_URL = "https://id.twitch.tv/oauth2/token";
+const TWITCH_USERS_URL = "https://api.twitch.tv/helix/users?";
 
 const NGROK_URL = "https://3939-2603-7000-3800-81f0-00-a040.ngrok.io";
 
@@ -7,4 +9,9 @@ process.env.NODE_ENV !== "production"
   ? (SL_WEBHOOK_URL = NGROK_URL + "/webhooks/callback")
   : (SL_WEBHOOK_URL = "/webhooks/callback");
 
-module.exports = { TWITCH_SUB_URL, SL_WEBHOOK_URL };
+module.exports = {
+  TWITCH_SUB_URL,
+  TWITCH_AUTH_URL,
+  TWITCH_USERS_URL,
+  SL_WEBHOOK_URL,
+};
---
+++ server/middleware/auth.js
@@ -0,0 +1,52 @@
+const axios = require("axios").default;
+const config = require("config");
+const TWITCH_CLIENT_SECRET = config.get("TWITCH_CLIENT_SECRET");
+const TWITCH_CLIENT_ID = config.get("TWITCH_CLIENT_ID");
+const { TWITCH_AUTH_URL, TWITCH_USERS_URL } = require("../constants/urls");
+
+const auth = async (req, res, next) => {
+  const code = req.body.code;
+  if (!code) return res.status(400).send("No auth code provided");
+
+  // Get user token from Twitch, uses code from client Twitch login
+  try {
+    const twitchResponse = await axios.post(buildUserTokenUrl(req));
+    const token = twitchResponse.data.access_token;
+
+    // TODO: validate auth token, send bad response if incorrect
+
+    const user = await axios.get(TWITCH_USERS_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Client-Id": TWITCH_CLIENT_ID,
+      },
+    });
+
+    const userData = user.data.data[0];
+    req.body.userData = userData;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+};
+
+const buildUserTokenUrl = (req) => {
+  const clientIdParam = `?client_id=${TWITCH_CLIENT_ID}`;
+  const clientSecretParam = `&client_secret=${TWITCH_CLIENT_SECRET}`;
+  const codeParam = `&code=${req.body.code}`;
+  const grantType = `&grant_type=authorization_code`;
+  // need the right redirect
+  const redirectUri = `&redirect_uri=${req.headers.origin}/auth`;
+
+  return (
+    TWITCH_AUTH_URL +
+    clientIdParam +
+    clientSecretParam +
+    codeParam +
+    grantType +
+    redirectUri
+  );
+};
+
+module.exports = auth;
