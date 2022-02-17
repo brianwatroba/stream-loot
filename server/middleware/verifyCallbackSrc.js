639730ddc411f5dea1b0dcd343da3d2ffed6b3dd
brian watroba <brianwatroba@gmail.com>
2022-02-17 02:25:16 -0500

    code cleanup

--- server/middleware/verifyCallbackSrc.js
+++ server/middleware/verifyCallbackSrc.js
@@ -6,7 +6,6 @@ const {
   TWITCH_MESSAGE_TYPE,
   MESSAGE_TYPE_VERIFICATION,
   MESSAGE_TYPE_NOTIFICATION,
-  MESSAGE_TYPE_REVOCATION,
 } = require("../constants/headers");
 
 const verifyCallbackSrc = (req, res, next) => {
@@ -17,39 +16,25 @@ const verifyCallbackSrc = (req, res, next) => {
 
   if (verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
     const notification = req.body;
-
-    if (MESSAGE_TYPE_NOTIFICATION === req.headers[TWITCH_MESSAGE_TYPE]) {
+    if (req.headers[TWITCH_MESSAGE_TYPE] === MESSAGE_TYPE_NOTIFICATION) {
       res.sendStatus(204);
       next();
-    } else if (MESSAGE_TYPE_VERIFICATION === req.headers[TWITCH_MESSAGE_TYPE]) {
+    } else if (req.headers[TWITCH_MESSAGE_TYPE] === MESSAGE_TYPE_VERIFICATION) {
       res.status(200).send(notification.challenge);
-    } else if (MESSAGE_TYPE_REVOCATION === req.headers[TWITCH_MESSAGE_TYPE]) {
-      res.sendStatus(204);
-      console.log(`${notification.subscription.type} notifications revoked!`);
-      console.log(`reason: ${notification.subscription.status}`);
-      console.log(
-        `condition: ${JSON.stringify(
-          notification.subscription.condition,
-          null,
-          4
-        )}`
-      );
     } else {
       res.sendStatus(204);
-      console.log(`Unknown message type: ${req.headers[TWITCH_MESSAGE_TYPE]}`);
     }
   } else {
-    // Signatures didn't match.
     console.log("403");
     res.sendStatus(403);
   }
 };
 
-const getHmacMessage = (request) => {
+const getHmacMessage = (req) => {
   return (
-    request.headers[TWITCH_MESSAGE_ID] +
-    request.headers[TWITCH_MESSAGE_TIMESTAMP] +
-    JSON.stringify(request.body)
+    req.headers[TWITCH_MESSAGE_ID] +
+    req.headers[TWITCH_MESSAGE_TIMESTAMP] +
+    JSON.stringify(req.body)
   );
 };
 
@@ -57,8 +42,8 @@ const getHmac = (secret, message) => {
   return crypto.createHmac("sha256", secret).update(message).digest("hex");
 };
 
-const verifyMessage = (hmac, sig) => {
-  return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(sig));
+const verifyMessage = (hmac, signature) => {
+  return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(signature));
 };
 
 module.exports = verifyCallbackSrc;
